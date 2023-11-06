d0895d1a98abb2050724f177b2a2424d1e0b5a8b
Tom Spencer <thomas.spencer2034@gmail.com>
2023-11-06 13:43:40 -0500

    Add process.argv to students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,3 +1,4 @@
+const args = process.argv.slice(2);
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -11,11 +12,12 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON students.cohort_id = cohorts.id
-LIMIT 5;
+WHERE cohorts.name LIKE '%${args[0]}%'
+LIMIT ${args[1]}
 `)
   .then(res => {
     res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
     });
   })
   .catch(err => console.error('query error', err.stack));
